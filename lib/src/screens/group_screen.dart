5ff03976ff22b7d31e34f653e52a0bc68d865b49
Bull <94058124+CesBull@users.noreply.github.com>
2026-06-08 15:31:21 -0500

    selection position

--- lib/src/screens/group_screen.dart
+++ lib/src/screens/group_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import '../../models/group.dart';
+import '../../models/team.dart';
 
-class GroupScreen extends StatelessWidget {
+class GroupScreen extends StatefulWidget {
   final Group group;
 
   const GroupScreen({
@@ -10,22 +11,75 @@ class GroupScreen extends StatelessWidget {
   });
 
   @override
+  State<GroupScreen> createState() => _GroupScreenState();
+}
+
+class _GroupScreenState extends State<GroupScreen> {
+
+  final List<Team> selectedTeams = [];
+
+  void selectTeam(Team team) {
+
+    if (selectedTeams.contains(team)) {
+      return;
+    }
+
+    if (selectedTeams.length < 3) {
+
+      setState(() {
+        selectedTeams.add(team);
+
+        team.position = selectedTeams.length;
+      });
+
+      if (selectedTeams.length == 3) {
+        assignFourthPlace();
+      }
+    }
+  }
+
+  void assignFourthPlace() {
+
+    final remaining = widget.group.teams.firstWhere(
+          (team) => !selectedTeams.contains(team),
+    );
+
+    remaining.position = 4;
+  }
+
+  int? getPosition(Team team) {
+    return team.position;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Grupo ${group.name}'),
+        title: Text('Grupo ${widget.group.name}'),
       ),
       body: ListView.builder(
-        itemCount: group.teams.length,
+        itemCount: widget.group.teams.length,
         itemBuilder: (context, index) {
-          final team = group.teams[index];
+
+          final team = widget.group.teams[index];
 
           return ListTile(
+            onTap: () => selectTeam(team),
+
             leading: Text(
               team.flag,
               style: const TextStyle(fontSize: 30),
             ),
+
             title: Text(team.name),
+
+            trailing: getPosition(team) != null
+                ? CircleAvatar(
+              child: Text(
+                '${getPosition(team)}',
+              ),
+            )
+                : null,
           );
         },
       ),
